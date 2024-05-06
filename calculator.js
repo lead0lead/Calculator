d76e24647d00447352a6a85125bbdababb0804ee
leader0leader <leander.hemmelskamp@proton.me>
2024-05-06 17:51:20 +0200

    Add basic operations
    Add function operate that takes an operator and 2 numbers, calls function to calculate result

---
+++ calculator.js
@@ -0,0 +1,34 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+let firstNumber;
+
+let secondNumber;
+
+let operator;
+
+function operate(operator, firstNumber, secondNumber){
+    switch (operator){
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
